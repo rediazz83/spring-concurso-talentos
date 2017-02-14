1ffbb50c94c0807793e08ce4e5e76b5f61f13551
Rodrigo Diaz <esteban.diaz@globant.com>
2017-02-14 18:41:36 -0300

    Septima version del proyecto concurso de talentos con spring (ejecucion de aspectos con anotaciones)

--- src/main/java/concursantes/Audiencia.java
+++ src/main/java/concursantes/Audiencia.java
@@ -1,28 +1,36 @@
 package concursantes;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component("publico")
+@Aspect
 public class Audiencia {
-	
+
+	@Pointcut("execution(* concursantes.Concursante.ejecutar(..))")
+	public void ejecutarShow() { }
+
+	@Around("ejecutarShow()")
 	public void monitorearShow(ProceedingJoinPoint joinpoint) {
 		try {
 			System.out.println("El show está por comenzar!! Favor de tomar asiento... ");
 			System.out.println("... tengan la amabilidad de apagar sus celulares");
-			
-			long horaInicio = System.currentTimeMillis();		
-			
+
+			long horaInicio = System.currentTimeMillis();
+
 			joinpoint.proceed();
 			Thread.sleep(1000);
-			
+
 			long horaTermino = System.currentTimeMillis();
-			
+
 			System.out.println("BRAVO!!! clap! clap! clap!");
 			System.out.println("(duracion del show: " + (horaTermino - horaInicio) + ")");
 		} catch (Throwable t) {
 			System.out.println("BUUUUUUU!!!! Que devuelvan el dinero!! Esto es un robo");
 		}
 	}
-	
+
 }
