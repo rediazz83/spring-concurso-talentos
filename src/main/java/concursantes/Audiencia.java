0231c00797368cc66d88f808a241962ce7620f83
Rodrigo Diaz <esteban.diaz@globant.com>
2017-02-14 18:33:26 -0300

    Sexta version del proyecto concurso de talentos con spring (se introduce around para AOP)

--- src/main/java/concursantes/Audiencia.java
+++ src/main/java/concursantes/Audiencia.java
@@ -1,22 +1,28 @@
 package concursantes;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.stereotype.Component;
 
 @Component("publico")
 public class Audiencia {
-	public void sentarse() {
-		System.out.println("El show está por comenzar!! Favor de tomar asiento... ");
-	}
-
-	public void apagarCelulares() {
-		System.out.println("... tengan la amabilidad de apagar sus celulares");
-	}
-
-	public void aplaudir() {
-		System.out.println("BRAVO!!! clap! clap! clap!");
+	
+	public void monitorearShow(ProceedingJoinPoint joinpoint) {
+		try {
+			System.out.println("El show está por comenzar!! Favor de tomar asiento... ");
+			System.out.println("... tengan la amabilidad de apagar sus celulares");
+			
+			long horaInicio = System.currentTimeMillis();		
+			
+			joinpoint.proceed();
+			Thread.sleep(1000);
+			
+			long horaTermino = System.currentTimeMillis();
+			
+			System.out.println("BRAVO!!! clap! clap! clap!");
+			System.out.println("(duracion del show: " + (horaTermino - horaInicio) + ")");
+		} catch (Throwable t) {
+			System.out.println("BUUUUUUU!!!! Que devuelvan el dinero!! Esto es un robo");
+		}
 	}
 	
-	public void devolucion() {
-		System.out.println("BUUUUUUU!!!! Que devuelvan el dinero!! Esto es un robo");
-	}
 }
